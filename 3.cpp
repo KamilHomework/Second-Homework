de3f965db33a0be5e6355f2ddbd5ead80500efd4
KamilHomework <114255453+KamilHomework@users.noreply.github.com>
2022-09-30 17:06:19 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int a[8][8],lim;
+
+int check(int f,int s, int x, int y, int count){
+
+    if (((x-f)>=0 && (x-f)<=7) && ((y-s)>=0 && (y-s)<=7)){
+        a[x-f][y-s]=count;
+        return 1;
+    }
+    return 0;
+}
+
+void game(int x, int y, int count) {
+
+    if (count<lim and check(2,1,x,y,count)){
+        game(x - 2, y - 1,(count + 1));
+    }
+    if (count<lim and check(2,-1,x,y,count)){
+        game(x-2,y+1,(count + 1));
+    }
+    if (count<lim and check(-2,1,x,y,count)){
+        game(x + 2, y - 1,(count + 1));
+    }
+    if (count<lim and check(-2,-1,x,y,count)){
+        game(x+2,y+1,(count + 1));
+    }
+    if (count<lim and check(1,2,x,y,count)){
+        game(x-1,y-2,(count + 1));
+    }
+    if (count<lim and check(1,-2,x,y,count)){
+        game(x-1,y+2,(count + 1));
+    }
+    if (count<lim and check(-1,2,x,y,count)){
+        game(x+1,y-2,(count + 1));
+    }
+    if (count<lim and check(-1,-2,x,y,count)){
+        game(x+1,y+2,(count + 1));
+    }
+}
+
+int main() {
+    int x,y;
+    cin >> x >> y >> lim;
+
+    a[x][y]=2;
+    game(x,y,0);
+    for (auto & i : a){
+        for (int j : i){
+            cout <<j << "  ";
+        }
+        cout << endl;
+    }
+}
